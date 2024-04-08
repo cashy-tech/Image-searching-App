701405ce74d701a8904961471ef97db7c4da3368
Carolyne Ndunge <carohndunge@gmail.com>
2024-04-08 23:25:29 +0300

    Handling form submission

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import SearchBar from "./components/search-bar";
+import searchImages from "./api";
+
 function App() {
-  return <div>App</div>;
+  const handleSubmit = async (term) => {
+    const result = await searchImages(term);
+  };
+  return (
+    <div>
+      <SearchBar onSubmit={handleSubmit} />
+    </div>
+  );
 }
 
 export default App;
--- src/api.js
+++ src/api.js
@@ -1,15 +1,15 @@
-import { Axios } from "axios";
+import Axios from "axios";
 
-const searchImages = async () => {
+const searchImages = async (term) => {
   const response = await Axios.get("https://api.unsplash.com/search/photos", {
     headers: {
       Authorization: "Client-ID 34xShMOLnN78PxB-J0VMMIZHj3f1AmBERfac_fzH6iI",
     },
     params: {
-      query: "cars",
+      query: term,
     },
   });
-  return response;
+  return response.data.results;
 };
 
 export default searchImages;
